df225da5784e62ce85fea9af31913427d1d40184
-3σ <yurippe@m3sigma.net>
2024-12-07 18:54:49 +0900

    multicritic sr checkpoint

--- runbox/agents/multi_critic_self_refine/_agent.py
+++ runbox/agents/multi_critic_self_refine/_agent.py
@@ -1,4 +1,5 @@
 from typing import Callable, TypeVar, Mapping, Any
+import os
 
 from langchain_openai import ChatOpenAI
 
@@ -14,11 +15,11 @@ type _SelfRefineRowResult = list[_BenchEvalResult]
 
 def _stop(response: str) -> bool:
     response_ = response.lower()
-    return "`stop`" in response_\
-        or "'stop'" in response_\
-        or '"stop"' in response_
+    return "`yes`" in response_\
+        or "'yes'" in response_\
+        or '"yes"' in response_
 
-class SelfRefineAgent[_BenchInput, _BenchOutput, _BenchEvalResult](
+class MultiCriticSelfRefineAgent[_BenchInput, _BenchOutput, _BenchEvalResult](
     SupportsBenchmark[_BenchInput, _BenchOutput, _BenchEvalResult, _SelfRefineRowResult]
 ):
     def __init__(
@@ -27,19 +28,46 @@ class SelfRefineAgent[_BenchInput, _BenchOutput, _BenchEvalResult](
         critic_config: ChatOpenAIConfig,
         refiner_config: ChatOpenAIConfig,
         main_prompt_path: str,
-        critic_prompt_path: str,
+        critic_prompts_dir_path: str,
+        agg_critic_prompt_path: str,
         refiner_prompt_path: str,
         add_extractor: ExtractorAdder,
         n_iter: int = 1
     ) -> None:
         self.main = load_chat_prompt_template_json(main_prompt_path)\
             | ChatOpenAI(**main_config)
-        self.critic = load_chat_prompt_template_json(critic_prompt_path)\
+        self.critics = [
+            load_chat_prompt_template_json(prompt_path)\
+                | ChatOpenAI(**critic_config)
+            for prompt_path in os.listdir(critic_prompts_dir_path)
+        ]
+        self.agg_critic = load_chat_prompt_template_json(agg_critic_prompt_path)\
             | ChatOpenAI(**critic_config)
         self.refiner = load_chat_prompt_template_json(refiner_prompt_path)\
             | ChatOpenAI(**refiner_config)
         self.parser = add_extractor(self.parse)
         self.n_iter = n_iter
+
+    def _run_critic(self, input: _BenchInput, initial_response: str) -> tuple[list[str], str, float, bool]:
+        responses = []
+        total_cost = 0
+
+        for critic in self.critics:
+            critic_response, critic_cost = invoke(
+                critic,
+                { **input, "initial_response": initial_response } # type: ignore
+            )
+            responses.append(critic_response)
+            total_cost += critic_cost
+
+        agg_response, agg_cost = invoke(
+            self.agg_critic,
+            { f"critic_{i}": res for i, res in enumerate(responses) }
+        )
+        total_cost += agg_cost
+        stop = _stop(agg_response)
+
+        return responses, agg_response, total_cost, stop
 
     def run(self, input: _BenchInput) -> dict:
         initial_response, initial_cost = invoke(self.main, input)
@@ -54,12 +82,10 @@ class SelfRefineAgent[_BenchInput, _BenchOutput, _BenchEvalResult](
         prediction = output["initial_prediction"]
         for _ in range(self.n_iter):
             if not stop:
-                critic_response, critic_cost = invoke(
-                    self.critic,
-                    { **input, "initial_response": initial_response } # type: ignore
-                )
-                stop = _stop(critic_response)
+                critic_responses, critic_response, critic_cost, stop\
+                    = self._run_critic(input, initial_response)
             else:
+                critic_responses = []
                 critic_response = "-"
                 critic_cost = 0
 
@@ -70,6 +96,7 @@ class SelfRefineAgent[_BenchInput, _BenchOutput, _BenchEvalResult](
                 )
                 refiner_prediction = self.parser(refiner_response)
                 output["iteration"].append({ # type: ignore
+                    "critic_details": critic_responses,
                     "critic_response": critic_response,
                     "refiner_response": refiner_response,
                     "refiner_prediction": refiner_prediction,
